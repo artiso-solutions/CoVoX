65667e74c83376d1c3da600e403702760c5c961c
kczornik <kczornik@artiso.com>
2021-02-04 09:45:01 +0100

    Created .NET5 Projects for Speech-To-Text and text translation

---
+++ CoVoX_POC/SampleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using SpeechToText;
+using TextTranslate;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                await Recognize();
+            }
+        }
+
+        private static async Task Recognize()
+        {
+            Console.WriteLine("Press <Return> to start recognition");
+            if (Console.ReadKey().Key != ConsoleKey.Enter) return;
+
+            Console.WriteLine("I'm listening...");
+            var speechRecognitionResult = SpeechRecognition.RecognizeSpeech();
+            Console.WriteLine($"Detected: {speechRecognitionResult.Text}");
+            Console.WriteLine(speechRecognitionResult.DetectedLanguage == "en-US"
+                ? "English detected, no need to translate"
+                : $"Translated: {await TextTranslation.TranslateToEn(speechRecognitionResult.Text)}");
+        }
+    }
+}
