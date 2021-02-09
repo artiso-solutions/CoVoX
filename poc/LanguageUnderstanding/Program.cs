fce7aa76f33eb218c0889cba5b28852bc35323c6
Alexander Kauntz <akauntz@artiso.com>
2021-02-09 14:52:08 +0100

    changes

--- poc/LanguageUnderstanding/Program.cs
+++ poc/LanguageUnderstanding/Program.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Helpers;
 
 namespace LanguageUnderstanding
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // Change to your LUIS app Id
             string luisAppId = "dd8e2d7d-2ce4-49dd-afa2-375785f0d61e";
@@ -21,7 +24,7 @@ namespace LanguageUnderstanding
             //test
             var intent = "hi";
 
-            string region = Luis.GetResult(luisAppId, luisSubscriptionKey, intent);
+            string region = await Luis.GetResult(luisAppId, luisSubscriptionKey, intent).ConfigureAwait(false);
             Console.Write(string.Format("\n\rLUIS region: {0} \n\r", region));
             Console.Write("\nPress any key to continue...");
             Console.Read();
@@ -29,7 +32,7 @@ namespace LanguageUnderstanding
     }
     static class Luis
     {
-        public static string GetResult(string appIdLUIS, string subscriptionKeyLUIS, string intent)
+        public static async Task<string> GetResult(string appIdLUIS, string subscriptionKeyLUIS, string intent)
         {
             if (String.IsNullOrEmpty(appIdLUIS) || String.IsNullOrEmpty(subscriptionKeyLUIS)) return string.Empty;
 
@@ -41,28 +44,31 @@ namespace LanguageUnderstanding
 
             using (var client = new HttpClient())
             {
-                    var url = "https://covox-cognitive-service.cognitiveservices.azure.com/luis/prediction/v3.0/apps/dd8e2d7d-2ce4-49dd-afa2-375785f0d61e/slots/staging/predict?subscription-key=d1510f61f03747969f823ef1e8cf39ac&verbose=true&show-all-intents=true&log=true&query=";
-                    var urlWithIntent = url += intent;
-                  
-                    try
+                var url = "https://covox-cognitive-service.cognitiveservices.azure.com/luis/prediction/v3.0/apps/dd8e2d7d-2ce4-49dd-afa2-375785f0d61e/slots/staging/predict?subscription-key=d1510f61f03747969f823ef1e8cf39ac&verbose=true&show-all-intents=true&log=true&query=";
+                var urlWithIntent = url += intent;
+
+                try
+                {
+                    using (var response = client.GetAsync(urlWithIntent, HttpCompletionOption.ResponseContentRead, CancellationToken.None).Result)
                     {
-                        using (var response = client.GetAsync(urlWithIntent, HttpCompletionOption.ResponseContentRead, CancellationToken.None).Result)
+                        if (response.StatusCode.Equals(HttpStatusCode.OK))
                         {
-                            if (response.StatusCode.Equals(HttpStatusCode.OK))
-                            {
-                                return "endpoint reached";
-                            }
-                            else
-                            {
-                                Debugger.Log(0, "", "401 " + url + "\n\r");
-                            }
+                            var responseBody = await response.Content.ReadAsStringAsync();
+
+                            //TODO: fix Response object
+                            var result = JsonConvert.DeserializeObject<List<Response>>(responseBody);
+                        }
+                        else
+                        {
+                            Debugger.Log(0, "", "401 " + url + "\n\r");
                         }
                     }
-                    catch (Exception ex)
-                    {
-                        Debugger.Log(0, "exception", ex.Message);
-                    }
                 }
+                catch (Exception ex)
+                {
+                    Debugger.Log(0, "exception", ex.Message);
+                }
+            }
 
             return String.Empty;
         }
---
+++ poc/LanguageUnderstanding/Response.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+namespace LanguageUnderstanding
+{
+    public class Response
+    {
+        [JsonProperty("query")]
+        public string Query { get; set; }
+
+        [JsonProperty("prediction")]
+        public Predicion Prediction { get; set; }
+
+        //[JsonProperty("score")]
+        //public double Score { get; set; }
+    }
+    public class Predicion
+    {
+        [JsonProperty("topIntent")]
+        public string TopIntent { get; set; }
+
+        [JsonProperty("intents")]
+        public List<Score> Score { get; set; }
+    }
+
+    public class Score
+    {
+        [JsonProperty("")]
+        public double IntentName { get; set; }
+
+        [JsonProperty("score")]
+        public double ScoreValue { get; set; }
+    }
+}
