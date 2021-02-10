68e4a7e92b482a2a81efb148f1a3bc8a44517970
kczornik <kczornik@artiso.com>
2021-02-10 09:58:51 +0100

    added post fill in-memory stream demo with delay;

--- poc/demos/single-speech-to-translated-text/README.md
+++ poc/demos/single-speech-to-translated-text/README.md
@@ -1,5 +1,5 @@
 ## Single Speech-To-Text with Translation
-Sample app that showcases how a complete integration from speech to translated text could work.
+Sample app that showcases how a complete integration of speech to translated text could work.
 
 ### Requirements
 Provide subscription keys for ACS Translator and ACS Speech
--- poc/listening/inmemorystream/Program.cs
+++ poc/listening/inmemorystream/Program.cs
@@ -15,8 +15,43 @@ namespace SpeechToTextFromInMemoryStream
 
         static async Task Main(string[] args)
         {
+            var showMenu = true;
+            while (showMenu)
+            {
+                showMenu = await Menu();
+            }
+        }
+
+        private static async Task<bool> Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose type of in-memory stream:");
+            Console.WriteLine("1) Pre-filled");
+            Console.WriteLine("2) Post-filled");
+            Console.WriteLine("3) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    await RecognizePreFilledStream();
+                    Console.Write("\r\nPress Enter to return to the menu");
+                    Console.ReadLine();
+                    return true;
+                case "2":
+                    await RecognizePostFilledStream();
+                    Console.Write("\r\nPress Enter to return to the menu");
+                    Console.ReadLine();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private static async Task RecognizePreFilledStream()
+        {
             var reader = new BinaryReader(File.OpenRead("whatstheweatherlike.wav"));
-            using var audioInputStream = AudioInputStream.CreatePushStream();
+            var audioInputStream = AudioInputStream.CreatePushStream();
 
             byte[] readBytes;
             do
@@ -27,7 +62,41 @@ namespace SpeechToTextFromInMemoryStream
 
             var result = await SpeechRecognition.RecognizeSpeechFromStream(SubscriptionKey, Region, audioInputStream, AutoDetectSourceLanguageConfig);
             Console.WriteLine($"Recognized '{result.DetectedLanguage}': {result.Text}");
-            Console.ReadLine();
+        }
+
+        private static async Task RecognizePostFilledStream()
+        {
+            var speechConfig = SpeechConfig.FromSubscription(SubscriptionKey, Region);
+            
+            var audioInputStream = AudioInputStream.CreatePushStream();
+            
+            using var audioConfig = AudioConfig.FromStreamInput(audioInputStream);
+            using var recognizer = new SpeechRecognizer(speechConfig, AutoDetectSourceLanguageConfig, audioConfig);
+
+            var recognitionTask = recognizer.RecognizeOnceAsync();
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            var fillAudioStreamTask = Task.Run(() =>
+            {
+                var reader = new BinaryReader(File.OpenRead("whatstheweatherlike.wav"));
+
+                byte[] readBytes;
+                do
+                {
+                    readBytes = reader.ReadBytes(1024);
+                    audioInputStream.Write(readBytes, readBytes.Length);
+                } while (readBytes.Length > 0);
+            });
+
+            await Task.WhenAll(recognitionTask, fillAudioStreamTask);
+
+            if (recognitionTask.Result.Reason == ResultReason.Canceled)
+                throw new Exception("Api Communication Failure");
+
+            var detectedLanguage = AutoDetectSourceLanguageResult.FromResult(recognitionTask.Result).Language;
+            
+            Console.WriteLine($"Recognized '{detectedLanguage}': {recognitionTask.Result.Text}");
         }
     }
 }
