35491c3bf5db51d14e03b7b9f05201415ec0b9ab
abruno <abruno@artiso.com>
2021-02-11 14:42:40 +0100

    multiple continuous recognition tryouts

--- poc/listening/microphone/SpeechRecognition.cs
+++ poc/listening/microphone/SpeechRecognition.cs
@@ -1,25 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
+using TextTranslate;
 
 namespace SpeechToTextFromMic
 {
     public class SpeechRecognition
     {
-        public static async Task<(string Text, string DetectedLanguage)> RecognizeSpeech(string subscriptionKey, string region)
+        private readonly string _subscriptionKey;
+        private readonly string _region;
+        private readonly Dictionary<string, SpeechRecognizer> _recognizers = new();
+        private readonly Dictionary<string, Stopwatch> _stopwatches = new();
+
+        public SpeechRecognition(string subscriptionKey, string region, List<string> languageConfigs)
         {
+            _subscriptionKey = subscriptionKey;
+            _region = region;
             var config = SpeechConfig.FromSubscription(subscriptionKey, region);
-            var autoDetectSourceLanguageConfig = AutoDetectSourceLanguageConfig.FromOpenRange();
-            var recognizer = new SpeechRecognizer(config, autoDetectSourceLanguageConfig);
-            var result = await recognizer.RecognizeOnceAsync();
+            
+            foreach (var languageConfig in languageConfigs)
+            {
+                var instanceCreation = Stopwatch.StartNew(); 
+                Console.WriteLine($"SpeechRecognizer instance creation : {instanceCreation.ElapsedMilliseconds} ms");
 
-            if (result.Reason == ResultReason.Canceled)
-                throw new Exception("Api Communication Failure");
+                var recognizer = new SpeechRecognizer(config, AutoDetectSourceLanguageConfig.FromLanguages(new[] {languageConfig}));
+                var stopWatch = new Stopwatch();
+                
+                _recognizers.Add(languageConfig, recognizer);
+                _stopwatches.Add(languageConfig, stopWatch);
+                
+                recognizer.SessionStarted += (_, _) =>
+                {
+                    // Console.WriteLine($"SessionStarted ({languageConfig}): {stopWatch.ElapsedMilliseconds} ms");
+                };
+            
+                recognizer.SpeechStartDetected += (_, _) =>
+                {
+                    stopWatch.Start();
+                    // Console.WriteLine($"SpeechStartDetected ({languageConfig}): {stopWatch.ElapsedMilliseconds} ms");
+                };
 
-            var detectedLanguage = AutoDetectSourceLanguageResult.FromResult(result).Language;
-            return (result.Text, detectedLanguage);
+                recognizer.Recognizing += (_, _) =>
+                {
+                    // TODO : Could be published the recognizing
+                    // Console.WriteLine($"Recognizing ({languageConfig}) ..... : {stopWatch.ElapsedMilliseconds} ms");
+                    stopWatch.Restart();
+                };
+                
+                Console.WriteLine($"SpeechRecognizer instance creation ({languageConfig}): {instanceCreation.ElapsedMilliseconds} ms");
+                instanceCreation.Stop();
+            }
+            
         }
+        
+        public async Task RecognizeSpeechContinuous(string language)
+        {
+            var recognizer = _recognizers[language];
+            var stopWatch = _stopwatches[language];
 
+            await recognizer.StartContinuousRecognitionAsync();
+
+            recognizer.Recognized += (sender, args) =>
+            {
+                var translationResult = TextTranslation.TranslateToEn(_subscriptionKey, _region, args.Result.Text).GetAwaiter().GetResult();
+                var translation = translationResult.FirstOrDefault();
+
+                if (!string.IsNullOrWhiteSpace(args.Result.Text))
+                {
+                    Console.WriteLine($"{language} - Detected: {args.Result.Text} - Translated: {translation?.Translations.FirstOrDefault()?.Text} ({translation?.DetectedLanguage.Language}) ({stopWatch.ElapsedMilliseconds} ms)");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+
+                stopWatch.Restart();
+            };
+        }
+        
         public static async Task<(string Text, string DetectedLanguage)> RecognizeSpeech(string subscriptionKey, string region, AutoDetectSourceLanguageConfig autoDetectSourceLanguageConfig)
         {
             var config = SpeechConfig.FromSubscription(subscriptionKey, region);
