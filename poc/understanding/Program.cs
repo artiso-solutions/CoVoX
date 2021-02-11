3d3e209905af50dba2098437019d1b2051b54df9
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2021-02-11 17:36:06 +0100

    (minor) Cleanup.

--- poc/understanding/Program.cs
+++ poc/understanding/Program.cs
@@ -1,33 +1,28 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
-using System.Web.Helpers;
+using Newtonsoft.Json;
 
 namespace LanguageUnderstanding
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static async Task Main()
         {
             // Change to your LUIS app Id
-            string luisAppId = "dd8e2d7d-2ce4-49dd-afa2-375785f0d61e";
+            string luisAppId = "AppId";
 
             // Change to your LUIS subscription key
-            string luisSubscriptionKey = "d1510f61f03747969f823ef1e8cf39ac";
+            string luisSubscriptionKey = "SubscriptionKey";
 
             //test
             var intent = "hi";
 
-            string region = await Luis.GetResult(luisAppId, luisSubscriptionKey, intent).ConfigureAwait(false);
+            string score = await Luis.GetResult(luisAppId, luisSubscriptionKey, intent).ConfigureAwait(false);
             Console.WriteLine("Your Intent is: " + intent);
-            Console.Write(string.Format("\n\rWith a Score of: {0} \n\r", region));
+            Console.Write(string.Format("\n\rWith a Score of: {0} \n\r", score));
             Console.Write("\nPress any key to continue...");
             Console.Read();
         }
@@ -36,46 +31,28 @@ namespace LanguageUnderstanding
     {
         public static async Task<string> GetResult(string appIdLUIS, string subscriptionKeyLUIS, string intent)
         {
-            if (String.IsNullOrEmpty(appIdLUIS) || String.IsNullOrEmpty(subscriptionKeyLUIS)) return string.Empty;
+            if (string.IsNullOrEmpty(appIdLUIS) || string.IsNullOrEmpty(subscriptionKeyLUIS)) return null;
 
-            // queryString
-            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            var url = $"https://covox-cognitive-service.cognitiveservices.azure.com/luis/prediction/v3.0/apps/{appIdLUIS}/slots/staging/predict?subscription-key={subscriptionKeyLUIS}&verbose=true&show-all-intents=true&log=true&query={intent}";
 
-            //todo format url with these
-            string appIdWithSubscriptionKey = string.Join("|", appIdLUIS, subscriptionKeyLUIS);
+            using var client = new HttpClient();
+            using var response = await client.GetAsync(url);
 
-            using (var client = new HttpClient())
+            if (response.IsSuccessStatusCode)
             {
-                var url = "https://covox-cognitive-service.cognitiveservices.azure.com/luis/prediction/v3.0/apps/dd8e2d7d-2ce4-49dd-afa2-375785f0d61e/slots/staging/predict?subscription-key=d1510f61f03747969f823ef1e8cf39ac&verbose=true&show-all-intents=true&log=true&query=";
-                var urlWithIntent = url += intent;
+                var responseBody = await response.Content.ReadAsStringAsync();
 
-                try
-                {
-                    using (var response = client.GetAsync(urlWithIntent, HttpCompletionOption.ResponseContentRead, CancellationToken.None).Result)
-                    {
-                        if (response.StatusCode.Equals(HttpStatusCode.OK))
-                        {
-                            var responseBody = await response.Content.ReadAsStringAsync();
+                // TODO: fix Response object
+                var result = JsonConvert.DeserializeObject<Response>(responseBody);
 
-                            //TODO: fix Response object
-                            var result = JsonConvert.DeserializeObject<Response>(responseBody);
-
-                            return result.Prediction.Intents.FirstOrDefault().Value.Score.ToString();
-
-                        }
-                        else
-                        {
-                            Debugger.Log(0, "", "401 " + url + "\n\r");
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Debugger.Log(0, "exception", ex.Message);
-                }
+                return result.Prediction.Intents.FirstOrDefault().Value.Score.ToString();
+            }
+            else
+            {
+                Console.WriteLine(response.StatusCode);
             }
 
-            return String.Empty;
+            return null;
         }
     }
 }
