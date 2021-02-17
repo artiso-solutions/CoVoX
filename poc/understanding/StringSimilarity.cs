de864a0992bf9ba97f9a7207dc09b6820f333a89
Alexander Kauntz <akauntz@artiso.com>
2021-02-17 08:23:42 +0100

    Added StringSimilarity Calculation

---
+++ poc/understanding/StringSimilarity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using F23.StringSimilarity;
+
+namespace LanguageUnderstanding
+{
+    public static class StringSimilarity
+    {
+        public static string GetSimilarityLevenshtein(string input, string target)
+        {
+
+            var normalizedLevenshtein = new NormalizedLevenshtein();
+            return normalizedLevenshtein.Distance(input, target).ToString();
+            //Console.WriteLine(l.Distance("My string", "My $tring"));
+            //Console.WriteLine(l.Distance("My string", "My $tring"));
+            //Console.WriteLine(l.Distance("My string", "My $tring"));
+        }
+
+        public static string GetSimilarityNGram(string input, string target)
+        {
+            var ngram = new NGram(4);
+            return ngram.Distance(input, target).ToString();
+        }
+    }
+}
