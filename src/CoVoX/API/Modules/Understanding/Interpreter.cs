07e67e2dc733a243c8b18f3d3afd82b1656a091a
kczornik <kczornik@artiso.com>
2021-02-18 08:31:41 +0100

    namespace cleanup;

--- src/CoVoX/API/Modules/Understanding/Interpreter.cs
+++ src/CoVoX/API/Modules/Understanding/Interpreter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API.Modules.Understanding
+namespace API.Modules
 {
     public class Interpreter : IUnderstandingModule
     {
