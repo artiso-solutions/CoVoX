a4306ad18e2e55a02b75ee21fbbdcce65c9e08bb
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2021-02-24 15:03:19 +0100

    Translating api demo as Scenario.

--- poc/translating/TextTranslation/Program.cs
+++ poc/translating/TextTranslation/Program.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace TextTranslate
 {
     class Program
     {
-        private static readonly string SubscriptionKey = "SubscriptionKey";
-        private static readonly string Region = "Region";
+        private static readonly string SubscriptionKey = "";
+        private static readonly string Region = "";
 
-        static async Task Main(string[] args)
+        static async Task Main()
         {
-            while (true)
-            {
-                Console.Write("Input text to translate to english: ");
-                var textToTranslate = Console.ReadLine();
-                var results = await TextTranslation.TranslateToEn(SubscriptionKey, Region, textToTranslate);
-                foreach (var result in results)
-                {
-                    Console.WriteLine($"Detected language: {result.DetectedLanguage.Language}");
-                    Console.WriteLine($"Confidence score: {result.DetectedLanguage.Score}");
-                    Console.WriteLine($"Translated: {result.Translations.First().Text}");
-                }
-            }
+            await RunApiDemoScenario();
         }
+
+        public static async Task RunApiDemoScenario() =>
+            await new ApiDemoScenario(SubscriptionKey, Region).Run();
     }
 }
---
+++ poc/translating/TextTranslation/Scenarios/ApiDemoScenario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Shared;
+
+namespace TextTranslate
+{
+    class ApiDemoScenario : AbstractScenario
+    {
+        public ApiDemoScenario(string subscriptionKey, string region)
+            : base(subscriptionKey, region)
+        {
+        }
+
+        public async Task Run(CancellationToken cancellationToken = default)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.Write("Input text to translate to english: ");
+                var textToTranslate = Console.ReadLine();
+                var results = await TextTranslation.TranslateToEn(SubscriptionKey, Region, textToTranslate);
+                
+                foreach (var result in results)
+                {
+                    Console.WriteLine($"Detected language: {result.DetectedLanguage.Language}");
+                    Console.WriteLine($"Confidence score: {result.DetectedLanguage.Score}");
+                    Console.WriteLine($"Translated: {result.Translations.First().Text}");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
