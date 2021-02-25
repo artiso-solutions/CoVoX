057434de64d03f31963da93aecadc889b968618a
Joshua Schmid <jschmid@artiso.com>
2021-02-25 12:32:37 +0100

    Add CosineSimilarityInterpreter

---
+++ src/CoVoX/API/Modules/Understanding/Interpreters/CosineSimilarityInterpreter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Covox.Understanding;
+using F23.StringSimilarity;
+
+namespace Covox.Modules.Understanding.Interpreters
+{
+    public class CosineSimilarityInterpreter : IInterpreter
+    {
+        public double CalculateMatchScore(string target, string input)
+        {
+            var cosine = new Cosine(2);
+            var distance = cosine.Distance(input, target);
+            return 1.0 - distance;
+        }
+    }
+}
